55c32ad4397e6f4f3852461da1d28542e33029b5
fireness <firenessmax@gmail.com>
2015-05-17 15:09:46 -0300

    Algunos cambios

--- main.cpp
+++ main.cpp
@@ -14,30 +14,72 @@ private:
 	std::string buffer;
 public:
 	LAN(){pthread_mutex_init(&(this->Lmutex), NULL);}
-
 	void loadMessage(std::string msg){this->buffer=msg;}
 	std::string getMessage(){return this->buffer;}
-
 	//bloquear LAN
-	int tomarLinea(){return pthread_mutex_lock(&mylock);}
-	void soltarLinea(){return pthread_mutex_lock(&mylock);}
+	int tomarLinea(){return pthread_mutex_lock(&(this->Lmutex));}
+	void soltarLinea(){pthread_mutex_lock(&(this->Lmutex));}
 	//estas funciones equivalen a lock y unlock pero para una LAN especifica permitiendo declarar secciones criticas de LAN
+};
+class Equipo{
+   	private:
+   		LAN* l;
+   		std::string nombre;
+    		int E_id;
+   		pthread_t thread;
+   	public:
+		Equipo(int id,LAN* l);
+      	void start();
+      	void wait(){pthread_join(this->thread, NULL);}
+      	void reciveMessage();
+      	int getID(){return this->E_id;}
+};
+
+class Bridge{
+	pthread_t thread;
+	public:
+   	int B_id;
+   	LAN** redes;
+   	void start();
+	void wait(){pthread_join(this->thread, NULL);}
+   	Bridge(int id,LAN** l){this->B_id=id;this->redes=l;} 
 };
 
 //prototipo de funciones
 //la idea es hacer que las funciones que daban referencias cruzadas hacerlas al estilo C 
 
+//malas... editar
+void* startEquipo(void* arg);
+void* startBridge(void* arg);
+void sendMessage( Equipo* emisor,Equipo* receptor, std::string msg);
+/*
+void sendMessagethroughBridge(std::string msg,IPuente* p, Equipo* receptor);
+void repiteMessage(std::string msg, IEquipo* receptor);
+*/
+//fin de malas
 
-
-
+//puede que las use para la implementacion de start()
 int main(int argc, char const *argv[])
 {
-	nuevo A,B;
-	A.a=1;
-	B.a=2;
-	A>>&B;
-	std::cout<<B.a<<std::endl;
+	//<<--- inserte getopt aqui
 	return 0;
 }
 //clases
+void Equipo::start(){
+	int rc 	= 	pthread_create(&this->thread, NULL,startEquipo, NULL/*argumento*/ );
+    printf("Hello World! It's me, thread with argument %d!\n", this->E_id);
+}
+void Bridge::start(){
+	int rc 	= 	pthread_create(&this->thread, NULL,startBridge, NULL/*argumento*/ );
+	printf("Hello World! It's me, thread with argument %d!\n", this->B_id);
+}
+
 //funciones
+void* startEquipo(void* arg){
+	printf("uninplemented function\n");
+	return NULL;
+}
+void* startBridge(void* arg){
+	printf("uninplemented function\n");
+	return NULL;
+}
